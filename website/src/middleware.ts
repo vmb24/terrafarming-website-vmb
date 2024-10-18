fd1b4cd11e3b8e0bfba3e4ca83a403aea5685144
vynnydev <vynnyprudencio@gmail.com>
2024-10-18 16:56:13 -0300

    fix: added dockerfile changed aws account

--- website/src/middleware.ts
+++ website/src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { auth } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher([
   "/overview(.*)",
@@ -8,6 +9,16 @@ const isProtectedRoute = createRouteMatcher([
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) {
     auth().protect()
+
+    // const { userId } = auth();
+    // if (userId) {
+    //   // Chamar o Lambda para verificar/criar o usuário
+    //   await fetch('URL_DO_SEU_API_GATEWAY', {
+    //     method: 'POST',
+    //     body: JSON.stringify({ userId }),
+    //     headers: { 'Content-Type': 'application/json' }
+    //   });
+    // }
   }
 
   return NextResponse.next()
